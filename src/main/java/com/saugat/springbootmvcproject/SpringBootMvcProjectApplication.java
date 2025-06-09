9807e672c0aa9ebca644d30b82845215ff10858d
Saugat Thapa <saugatthapa2073@gmail.com>
2025-06-09 12:04:08 -0500

    Spring Boot MVC project initiated

---
+++ src/main/java/com/saugat/springbootmvcproject/SpringBootMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.saugat.springbootmvcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcProjectApplication.class, args);
+    }
+
+}
